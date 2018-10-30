aa5f13958be8b5d3ca2492a3319762c5480f60bb
Alex Rocha <alex.rochas@yahoo.com.br>
2018-10-30 01:01:29 -0200

    feat: first commit with framework poc

---
+++ cli.py
@@ -0,0 +1,6 @@
+from prompt_toolkit import prompt
+
+if __name__ == '__main__':
+    print('Welcome to Jenkins CLI')
+    answer = prompt('>>>')
+    print('Command %s not implemented yet..sorry' % answer)
